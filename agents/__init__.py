37016727d7fad46f446f3d5e980f19f81f081dc3
ِAmmar Alomran <116923794+AmmarAlomran@users.noreply.github.com>
2025-04-08 10:18:28 +0300

    add git ignore

--- agents/__init__.py
+++ agents/__init__.py
@@ -1,2 +1,7 @@
 from welcome_agent import WelcomeAgent
-from research_agent
+from research_agent import ResearchAgent
+from project_agent import ProjectAgent
+from client_agent import ClientAgent
+from career_agent import CareerAgent
+
+__all__ = ['WelcomeAgent', 'ResearchAgent', '']
